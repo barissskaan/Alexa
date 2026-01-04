b0f814d44ff348a3cab1e166b3215040f9ee9d54
Baris Kaan Datli <barissskaan@gmail.com>
2026-01-04 21:32:29 +0100

    .
    .

--- Core/Src/audio_processing.cpp
+++ Core/Src/audio_processing.cpp
@@ -9,38 +9,34 @@ extern "C" void HAL_I2S_RxHalfCpltCallback(I2S_HandleTypeDef* hi2s){
 		uint32_t temp = (uint32_t)buffer_input[i]<<16;
 		temp = temp | (uint32_t) buffer_input[i+1];
 		buffer_merged[i/4] = temp;
-		micflag = 1;
-		}
+	}
+	micflag = 1;
 	audioProcessingLeft();
-	}
-
-extern "C" void HAL_I2S_RxCpltCallback(I2S_HandleTypeDef* hi2s){
-	for(int i=2000 ;i<4000;i+=4){
-		uint32_t temp = (uint32_t)buffer_input[i]<<16;
-		temp = temp | (uint32_t) buffer_input[i+1];
-		buffer_merged[i/4] = temp;
-		micflag = 2;
-		}
-	audioProcessingRight();
-	}
+}
 
 void audioProcessingLeft(){
 	int sum = 0;
 	for(int i=0; i < 500; i++){
 		sum += abs((int)buffer_merged[i]>>14);
 	}
-	// led_func(sum/(500*780));
-
-
+	led_func(sum/(500*780));
 }
+
+extern "C" void HAL_I2S_RxCpltCallback(I2S_HandleTypeDef* hi2s){
+	for(int i=2000 ;i<4000;i+=4){
+		uint32_t temp = (uint32_t)buffer_input[i]<<16;
+		temp = temp | (uint32_t) buffer_input[i+1];
+		buffer_merged[i/4] = temp;
+		}
+	micflag = 2;
+	audioProcessingRight();
+	}
+
 void audioProcessingRight(){
 	int sum = 0;
 	for(int i=500; i < 1000; i++){
 		sum += abs((int)buffer_merged[i]>>14);
 	}
-	// led_func(sum/(500*780));
-
-
-
+	led_func(sum/(500*780));
 }
 
