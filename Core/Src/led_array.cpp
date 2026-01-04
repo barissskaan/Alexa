1bbfadf6a08a2f3383702bcf500600180e3e824c
Baris Kaan Datli <barissskaan@gmail.com>
2026-01-04 23:43:08 +0100

    .
    .

--- Core/Src/led_array.cpp
+++ Core/Src/led_array.cpp
@@ -3,7 +3,7 @@
 static GPIO_TypeDef* port_arr[10] = {GPIOG,GPIOG,GPIOF,GPIOE,GPIOF,GPIOE,GPIOE,GPIOF,GPIOF,GPIOD};
 static uint16_t pin_arr[10] = {GPIO_PIN_9,GPIO_PIN_14,GPIO_PIN_15,GPIO_PIN_13,GPIO_PIN_14,GPIO_PIN_11,GPIO_PIN_9,GPIO_PIN_13,GPIO_PIN_12,GPIO_PIN_15};
 
-void led_func(int32_t led_count){
+extern "C" void led_func(int led_count){
 	led_count = led_count > 10 ? 10 : led_count < 0 ? 0 : led_count;
 
 	for(int i = 0; i<10; i++){
